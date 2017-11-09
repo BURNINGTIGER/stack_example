5269451b00b1a0dbc845619bf881c0f54623fecb
BURNINGTIGER <Aleks203467@gmail.com>
2017-11-09 23:25:32 +0300

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,22 +1,44 @@
-#include <iostream>
-#include <sstream>
-
 #include "stack.hpp"
+#include <string>
+#include <chrono>
+#include <thread>
+
+void producer(stack<int> &Stack)
+{
+	for (;;)
+	{
+		Stack.push(std::rand() % 100);
+		std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 1));
+	}
+}
+
+void consumer(stack<int> &Stack)
+{
+	for (;;)
+	{
+		try
+		{
+			Stack.pop();
+		}
+		catch (std::logic_error)
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 2));
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 2));
+	}
+}
 
 int main()
 {
-    stack<int> s;
-    for( std::string line; std::getline( std::cin, line ); ) {
-        std::istringstream stream{ line };
-        if( char op; stream >> op ) {
-            if( op == 'e' ) {
-                std::cout << std::boolalpha << s.empty();
-            } 
-            else if( op == 'q' ) {
-                break;
-            }
-        }
-    }
-    
-    return 0;
+	srand(time(NULL));
+	stack<int> Stack;
+
+	std::thread pr(producer, std::ref(Stack));
+	std::thread cn(consumer, std::ref(Stack));
+
+	pr.join();
+	cn.join();
+
+
+	return 0;
 }
